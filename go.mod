3495a9880d5f6451c1e2c0af931d360ef7e694e7
Lizzy <liza.guseva@hey.com>
2025-08-19 17:16:01 +0200

    can record audio

--- go.mod
+++ go.mod
@@ -22,12 +22,16 @@ require (
 	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
 	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
 	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/riff v1.0.0 // indirect
+	github.com/go-audio/wav v1.1.0 // indirect
 	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/go-text/render v0.2.0 // indirect
 	github.com/go-text/typesetting v0.2.1 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gordonklaus/portaudio v0.0.0-20250206071425-98a94950218b // indirect
 	github.com/hack-pad/go-indexeddb v0.3.2 // indirect
 	github.com/hack-pad/safejs v0.1.0 // indirect
 	github.com/jeandeaual/go-locale v0.0.0-20250612000132-0ef82f21eade // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"log/slog"
+	"os"
 	"strings"
 
 	"fyne.io/fyne/v2"
@@ -11,6 +13,9 @@ import (
 	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 	"github.com/atotto/clipboard"
+	"github.com/go-audio/audio"
+	"github.com/go-audio/wav"
+	"github.com/gordonklaus/portaudio"
 )
 
 //go:embed icon.png
@@ -23,27 +28,33 @@ var MainWindowWidth float32 = 600
 var GapHeight float32 = 50
 
 func main() {
+	// general setup
 	fmt.Println("Hello, 世界")
 	fyneApp = app.New()
 	fyneApp.SetIcon(fyne.NewStaticResource("icon", iconData))
 	clipboardEntry.SetText("!")
 
+	//audio
+	stopChan := make(chan struct{})
+
+	// main window setup
 	mainWindow := fyneApp.NewWindow("SomeHelp Assistant")
 	mainWindow.Resize(fyne.NewSize(MainWindowWidth, 120))
 	mainWindow.SetFixedSize(false)
-	setupMainWindow(mainWindow)
+	setupMainWindow(mainWindow, stopChan)
 	mainWindow.Hide()
 
-	setupSystemTray(mainWindow)
+	//system tray and run
+	setupSystemTray(mainWindow, stopChan)
 	fyneApp.Run()
 }
 
-func setupSystemTray(mainWindow fyne.Window) {
+func setupSystemTray(mainWindow fyne.Window, stopChan chan struct{}) {
 	desk, _ := fyneApp.(desktop.App)
 	fmt.Println("Setting up system tray...")
 	menu := fyne.NewMenu("SomeHelp?",
 		fyne.NewMenuItem("Record your question", func() {
-			showRecordingWindow(mainWindow)
+			showRecordingWindow(mainWindow, stopChan)
 		}),
 		fyne.NewMenuItemSeparator(),
 		fyne.NewMenuItem("Close record window", func() {
@@ -54,7 +65,7 @@ func setupSystemTray(mainWindow fyne.Window) {
 	desk.SetSystemTrayIcon(fyne.NewStaticResource("icon", iconData))
 }
 
-func setupMainWindow(window fyne.Window) {
+func setupMainWindow(window fyne.Window, stopChan chan struct{}) {
 	topLabel := widget.NewLabel("SomeHelp? Assistant")
 	window.SetCloseIntercept(func() {
 		window.Hide()
@@ -64,15 +75,20 @@ func setupMainWindow(window fyne.Window) {
 	scrollContainer := container.NewScroll(clipboardEntry)
 	scrollContainer.SetMinSize(fyne.NewSize(MainWindowWidth-20, 30))
 
+	stopButton := widget.NewButton("Stop", func() {
+		stopChan <- struct{}{}
+	})
+
 	content := container.NewVBox(
 		topLabel,
+		stopButton,
 		widget.NewSeparator(),
 		scrollContainer,
 	)
 	window.SetContent(content)
 }
 
-func showRecordingWindow(mainWindow fyne.Window) {
+func showRecordingWindow(mainWindow fyne.Window, stopChan chan struct{}) {
 	mainWindow.Hide()
 	clipboardEntry.SetText(readClipboard())
 	lines := strings.Count(clipboardEntry.Text, "\n") + 1
@@ -83,6 +99,8 @@ func showRecordingWindow(mainWindow fyne.Window) {
 	}
 	height := GapHeight + float32(lines*22)
 	statusLabel := widget.NewLabel("🎙️ Recording...")
+	recording := make([]int16, 0)
+	go listen(stopChan, &recording)
 
 	clipboardContent := widget.NewEntry()
 	clipboardContent.MultiLine = true
@@ -91,8 +109,16 @@ func showRecordingWindow(mainWindow fyne.Window) {
 	scrollContainer := container.NewScroll(clipboardContent)
 	scrollContainer.SetMinSize(fyne.NewSize(MainWindowWidth-20, height-GapHeight+25))
 	mainWindow.Resize(fyne.NewSize(500, height))
+
+	stopButton := widget.NewButton("⏹️ Stop Recording", func() {
+		stopChan <- struct{}{}
+		statusLabel.SetText("✅ Recording stopped")
+		saveToWAV(recording, "recording.wav")
+	})
+
 	content := container.NewVBox(
 		statusLabel,
+		stopButton,
 		widget.NewSeparator(),
 		scrollContainer,
 	)
@@ -105,8 +131,84 @@ func showRecordingWindow(mainWindow fyne.Window) {
 func readClipboard() string {
 	text, err := clipboard.ReadAll()
 	if err != nil {
-		fmt.Println("Error reading clipboard:", err)
+		slog.Error("Error reading clipboard", "error", err)
 		return ""
 	}
 	return text
+}
+
+func listen(stopChan chan struct{}, recording *[]int16) error {
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+
+	inputDevice, err := portaudio.DefaultInputDevice()
+	if err != nil {
+		slog.Error("Error getting default input device", "error", err)
+		return err
+	}
+
+	inputParams := portaudio.StreamParameters{
+		Input: portaudio.StreamDeviceParameters{
+			Device:   inputDevice,
+			Channels: 1,
+			Latency:  inputDevice.DefaultLowInputLatency,
+		},
+		SampleRate:      44100,
+		FramesPerBuffer: 1024,
+	}
+	buffer := make([]int16, 1024)
+	stream, err := portaudio.OpenStream(inputParams, buffer)
+	if err != nil {
+		slog.Error("Error opening stream", "error", err)
+		return err
+	}
+
+	err = stream.Start()
+	if err != nil {
+		slog.Error("Error starting stream", "error", err)
+		return err
+	}
+
+	for {
+		select {
+		case <-stopChan:
+			stream.Stop()
+			stream.Close()
+			return nil
+		default:
+			err = stream.Read()
+			if err != nil {
+				slog.Error("Error reading stream", "error", err)
+				return err
+			}
+			*recording = append(*recording, buffer...)
+		}
+	}
+}
+
+func saveToWAV(data []int16, filename string) error {
+	convertedData := make([]int, len(data))
+	for i, v := range data {
+		convertedData[i] = int(v)
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := wav.NewEncoder(file, 44100, 16, 1, 1)
+	if err != nil {
+		return err
+	}
+	defer encoder.Close()
+
+	buffer := audio.IntBuffer{
+		Data: convertedData,
+		Format: &audio.Format{
+			SampleRate:  44100,
+			NumChannels: 1,
+		},
+	}
+	return encoder.Write(&buffer)
 }
